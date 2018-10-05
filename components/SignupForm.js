35ccb4cf5fca6487faeddf1e745737637a35ac16
Chase McCoy <chasem000@gmail.com>
2018-10-04 19:44:11 -0500

    add signup form

---
+++ components/SignupForm.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const SignupForm = () => (
+	<div id="mc_embed_signup" className='box roomy'>
+		<form
+			action="https://chasem.us19.list-manage.com/subscribe/post?u=b204146be746a61ff69c861f3&amp;id=d9e71d1941"
+			method="post"
+			id="mc-embedded-subscribe-form"
+			name="mc-embedded-subscribe-form"
+			className="validate"
+			target="_blank"
+		>
+			<div id="mc_embed_signup_scroll">
+				<div className="mc-field-group">
+					<label htmlFor="mce-EMAIL">Email Address</label>
+					<input type="email" defaultValue="" name="EMAIL" className="required email" id="mce-EMAIL" />
+				</div>
+
+				<div className="mc-field-group">
+					<label htmlFor="mce-FNAME">First Name</label>
+					<input type="text" defaultValue="" name="FNAME" className="" id="mce-FNAME" />
+				</div>
+
+				<div id="mce-responses" className="clear">
+					<div className="response" id="mce-error-response" style={{display: "none"}}></div>
+					<div className="response" id="mce-success-response" style={{display: "none"}}></div>
+				</div>
+
+				<div
+					style={{position: 'absolute', left: '-5000px'}}
+					aria-hidden="true"
+				>
+					<input type="text" name="b_b204146be746a61ff69c861f3_d9e71d1941" tabIndex="-1" defaultValue="" />
+				</div>
+
+				<div className="clear">
+					<input type="submit" defaultValue="Let's do this!" name="subscribe" id="mc-embedded-subscribe" className="button" />
+				</div>
+			</div>
+		</form>
+	</div>
+)
+
+export default SignupForm
